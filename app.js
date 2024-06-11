50e09d7222f1c35aff0db691d7744a6c7eb63e9d
zoeznm <bomikim046@gmail.com>
2024-06-11 11:30:15 +0900

    2교시

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-const a = () => {
-  return {
-    name : "",
-    age : 0,
-    job: "",
-  };
-};
+function formatHTML(language) {
+  let htmlData = {
+    titleOne: "공욱재",
+    titleTwo:"kongukjae",
+    h1:"내이름은",
+    p:"공욱재입니다",
+  }
 
-// console.log(a());
+  let decision = () => {
+    if(language === "korean") {
+      return htmlData.titleOne;
+    }
+    if(language === "english") {
+      return htmlData.titleTwo;
+    }
+  }
 
-const b = a();
-console.log(b);
+
+  let html = `
+  <html>
+    <head>
+      <title>${decision()}</title>
+    </head>
+    <body>
+      <h1>${htmlData.h1}</h1>
+      <p>${htmlData.p}</p>
+    </body>
+  </html>
+  `;
+  
+  return html;
+}
+
+console.log(formatHTML("korean"));
+console.log(formatHTML("english"));
